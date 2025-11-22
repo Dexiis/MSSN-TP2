af39d7eb58c157f8721289d7a30e64070c65fdca
David Santos <daapsyt@gmail.com>
2025-11-22 19:31:58 +0000

    wind and birds

--- Projeto2/src/TP2/Bodies/Soldier.java
+++ Projeto2/src/TP2/Bodies/Soldier.java
@@ -2,75 +2,62 @@ package TP2.Bodies;
 
 import TP2.Core.SubPlot;
 import processing.core.PApplet;
-import processing.core.PConstants;
 import processing.core.PImage;
 import processing.core.PVector;
 
 public class Soldier extends Body {
-
+	
 	private boolean parachuteOpen = false;
 	private int parachuteOpenHeight;
 	private final float initialRadius;
-
+	
 	private PImage soldierImage = null;
 	private PImage parachuteImage = null;
-
+	
 	public Soldier(PVector posititon, PVector velocity, float mass, float radius, int color, PApplet p) {
 		super(posititon, velocity, mass, radius, color);
 		this.initialRadius = radius;
-
+		
 		this.soldierImage = p.loadImage("Resources/Stickman.png");
 		this.parachuteImage = p.loadImage("Resources/Parachute.png");
-
+		
 	}
-
-	public void setParachuteHeight(int height) {
-		this.parachuteOpenHeight = height;
-	}
-
-	public void checkParachute() {
-		this.parachuteOpen = this.getPosition().y <= this.parachuteOpenHeight;
-	}
-
-	public boolean isParachuteOpen() {
-		return this.parachuteOpen;
-	}
-
-	public boolean isGrounded() {
-		return this.getPosition().y <= 10;
-	}
-
+	
+	public void setParachuteHeight(int height) { this.parachuteOpenHeight = height; }
+	public void checkParachute() { this.parachuteOpen = this.getPosition().y <= this.parachuteOpenHeight; }
+	public boolean isParachuteOpen() { return this.parachuteOpen; }
+	public boolean isGrounded() { return this.getPosition().y <= 10; }
 	public void updateRadius() {
-		if (isParachuteOpen())
+		if(isParachuteOpen())
 			this.radius = this.initialRadius + 5;
 	}
-
+	
 	@Override
 	public void display(PApplet p, SubPlot plt) {
 		p.pushStyle();
-
+		
 		positions = plt.getPixelCoord(position.x, position.y);
 
 		p.pushMatrix();
-
+		
 		// Set the anchor point to the center of the soldier
 		p.translate(positions[0], positions[1]);
-
-		if (!isGrounded()) {
-			if (!isParachuteOpen()) {
+		
+		if(!isGrounded()) {
+			if(!isParachuteOpen()) {
 				p.scale(1, -1);
 			} else {
-				p.imageMode(PConstants.CENTER);
+				p.imageMode(p.CENTER);
 				p.image(this.parachuteImage, 0, -20, 50, 50);
 			}
-		}
-
-		p.imageMode(PConstants.CENTER);
-		p.image(this.soldierImage, 0, 0, 25, 25);
-
+		} 
+		
+		p.imageMode(p.CENTER);
+		p.image(this.soldierImage, 0, 0, 30, 30);
+		
 		p.popMatrix();
 
 		p.popStyle();
 	}
-
+	
 }
